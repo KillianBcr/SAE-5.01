5cb40891463c2697eab264baeb29b73884c728e7
scho0029 <luc.schoeny@etudiant.univ-reims.fr>
2023-10-05 12:06:12 +0200

    end of session, data recovery problem

--- assets/react/components/Repartition.jsx
+++ assets/react/components/Repartition.jsx
@@ -3,7 +3,6 @@ import "../../styles/repartition.css";
 import { Link } from 'wouter';
 
 function Repartition() {
-
     const [wishes, setWishes] = useState([]);
 
     const handleDeleteWish = (wishId) => {
@@ -17,7 +16,6 @@ function Repartition() {
                 if (response.ok) {
                     setWishes(wishes.filter((wish) => wish.id !== wishId));
                 } else {
-
                     console.error('Erreur lors de la suppression du souhait');
                 }
             })
@@ -26,30 +24,48 @@ function Repartition() {
             });
     };
 
+    useEffect(() => {
+        fetch('/api/user/wishes', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Erreur lors de la récupération des souhaits');
+                }
+            })
+            .then((data) => {
+                setWishes(data);
+            })
+            .catch((error) => {
+                console.error('Erreur lors de la récupération des souhaits :', error);
+            });
+    }, []);
+
     return (
         <div className="table-container">
             <h2>Répartition de vos heures</h2>
             <table>
                 <thead>
                 <tr>
-                    <th>Cours</th>
-                    <th>Section</th>
-                    <th>Volume</th>
-                    <th>Modification</th>
                     <th>Wish ID</th>
+                    <th>Volume</th>
+                    <th>Actions</th>
                 </tr>
                 </thead>
                 <tbody>
                 {wishes.map((wish) => (
                     <tr key={wish.id}>
-                        <td>cours</td>
-                        <td>section</td>
-                        <td>volume heures</td>
+                        <td>{wish.id}</td>
+                        <td>{wish.subject.hoursTotal}</td>
                         <td>
                             <button className="modifier-button">Modifier</button>
                             <button className="supprimer-button" onClick={() => handleDeleteWish(wish.id)}>Supprimer</button>
                         </td>
-                        <td>{wish.id}</td>
                     </tr>
                 ))}
                 </tbody>
